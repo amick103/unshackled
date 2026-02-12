4d0728974d622ac52132cd7753f35b0080b6c10b
amick103 <jeff.amick@gmail.com>
2026-02-11 21:47:09 -0500

    Register service worker on window load

--- app.js
+++ app.js
@@ -308,3 +308,8 @@ function renderPlan() {
 }
 
 renderDebts();
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js").catch(() => {});
+  });
+}
